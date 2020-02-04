813072b5d1196f28fbcbb4aa6ab64c42475f0ea5
Yu Cao <cy3118018@163.com>
2020-02-04 21:10:37 +0800

    Initial Commit

---
+++ TvPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TvPlayer
+//
+//  Created by CaoYu on 2020/2/4.
+//  Copyright © 2020 Yu Cao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
