55e5a0d9a88fae07db10d2a6a90d0ba7dfda4eb7
Yu Cao <cy3118018@163.com>
2020-02-14 15:13:53 +0800

    initial commit

--- TvPlayer/ContentView.swift
+++ TvPlayer/ContentView.swift
@@ -7,11 +7,88 @@
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
+import Alamofire
+import SwiftyJSON
+
+var player: AVPlayer!
+let playerLayer = AVPlayerLayer()
+var stationListUrl: String = "http://13.78.120.63/tv/tv_station_list.json"
+
+func playStation(url: String) {
+    player = AVPlayer(url: URL(string: url)!)
+    playerLayer.player = player
+    player?.play()
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+          PlayerContainerView()
     }
+}
+
+struct PlayerView: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+    }
+    func makeUIView(context: Context) -> UIView {
+        return PlayerUIView()
+    }
+}
+
+class PlayerUIView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        playerLayer.player = player
+        layer.addSublayer(playerLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+        playerLayer.videoGravity = .resize
+    }
+}
+
+struct PlayerContainerView : View {
+    @ObservedObject var stationLoader = StationLoader(urlString: stationListUrl)
+    
+    var body: some View {
+        VStack {
+            Text("TV Player")
+                .bold()
+            PlayerView()
+                .frame(height: 211)
+            
+            List(stationLoader.stations) { station in
+                StationRow(station: station)
+            }
+        }
+    }
+}
+
+struct StationRow : View {
+    var station: Station
+
+    var body: some View {
+            Button(action: {
+                playStation(url: self.station.url)
+                }
+            ) {
+                Text(station.name)
+                    .multilineTextAlignment(.center)
+                    .padding(.leading, 20.0)
+        }
+    }
+}
+
+struct Station: Decodable, Identifiable {
+    var id = UUID()
+    var name, url : String
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ TvPlayer/StationLoader.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkingManager.swift
+//  TvPlayer
+//
+//  Created by CaoYu on 2020/2/14.
+//  Copyright © 2020 Yu Cao. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import Alamofire
+import SwiftyJSON
+
+class StationLoader: ObservableObject {
+    @Published var stations = [Station]()
+    
+    init(urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        var tempStationList: [Station] = []
+
+        Alamofire.request(url).responseJSON { (response) in
+            switch response.result {
+            case .success(let json):
+                print(json)
+                let jsonObj = JSON.init(json)
+                for station in jsonObj["stations"].arrayValue {
+                    tempStationList.append(
+                        Station(
+                            name: station["name"].stringValue,
+                            url: station["url"].stringValue
+                            )
+                    );
+                }
+                self.stations = tempStationList
+                break
+            case .failure(let error):
+                print("error:\(error)")
+                break
+            }
+        }
+    }
+}
